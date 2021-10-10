13a5de415dd354e968755a69bbd0fdcea2c602ae
KenshuHori <kenshuhori@gmail.com>
2021-10-10 15:53:03 +0900

    apiを投げられるように調整

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,14 +2,29 @@ import { Flex, Heading, Input, Button, useColorMode, useColorModeValue } from '@
 
 const IndexPage = () => {
   const { toggleColorMode } = useColorMode();
-  const formBackground = useColorModeValue("gray.100", "gray.700")
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+  const submit = async () => {
+    await fetch("/api/admin", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ "hoge": "fuga" }),
+    })
+    .then((res) => {
+      console.log("POSTリクエスト成功");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
       <Flex direction="column" background={formBackground} p={12} rounded={6}>
         <Heading mb={6}>Log in</Heading>
         <Input placeholder="kenshuhori@gmail.com" varient="Filled" mb={3} type="email" />
         <Input placeholder="******" varient="Fileed" mb={6} type="password" />
-        <Button colorScheme="teal" mb={6}>Log In</Button>
+        <Button onClick={submit} colorScheme="teal" mb={6}>Log In</Button>
         <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
       </Flex>
     </Flex>
---
+++ src/pages/api/admin/index.ts
@@ -0,0 +1,18 @@
+import nextConnect from 'next-connect';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const apiRoute = nextConnect({
+  onError(error, req: NextApiRequest, res: NextApiResponse) {
+    res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
+
+apiRoute.post(async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("Request to POST:api/admin/index has come!!")
+  res.status(200).json("Hello from api/admin/index.ts");
+});
+
+export default apiRoute;
