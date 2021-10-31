1f3928983dd7bba20cc326ea578b8a95c55969f2
KenshuHori <kenshuhori@gmail.com>
2021-11-01 00:18:57 +0900

    head要素でtitle指定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from 'next/app';
 import { AdminLayout } from '@/components/admin/layout';
 import { Layout } from '@/components/layout';
+import Head from 'next/head'
 import '@/styles/app.css'
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   if (router.route.startsWith("/admin")) {
     return (
       <ChakraProvider>
+        <Head>
+          <title>シェアカルふたこ管理</title>
+        </Head>
         <AdminLayout>
           <Component {...pageProps} />
         </AdminLayout>
@@ -16,6 +20,9 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   } else {
     return (
       <ChakraProvider>
+        <Head>
+          <title>シェアカルふたこ</title>
+        </Head>
         <Layout>
           <Component {...pageProps} />
         </Layout>
