982fd0702ad36bc7c5a376a88ce36d479b7de02c
KenshuHori <kenshuhori@gmail.com>
2021-11-03 12:07:15 +0900

    サブヘッダを調整

--- src/pages/admin/cultures/index.tsx
+++ src/pages/admin/cultures/index.tsx
@@ -16,17 +16,20 @@ import api from '@/utils/api'
 
 const CulturesPage = () => {
   const backGround = useColorModeValue("white", "gray.700")
+  const borderColor = useColorModeValue("gray.400", "white")
   return (
     <>
       <ContentHeader contentName="カルチャー" useNew="true" newPath="/admin/cultures/new"></ContentHeader>
       <Flex mt="12px" bg={backGround} fontSize="sm">
-        <Box p="8px"><Text>フィルター</Text></Box>
-        <Input w="300px" fontSize="sm" border="2px solid black" borderRadius="3xl" placeholder="タイトルまたは説明文"></Input>
+        <Box p="10px">フィルター</Box>
+        <Box p="6px">
+          <Input w="300px" fontSize="sm" size="sm" border="1px solid" borderRadius="3xl" borderColor={borderColor} placeholder="タイトルまたは説明文"></Input>
+        </Box>
         <Spacer />
-        <Box p="4px">並び順</Box>
-        <Box>
+        <Box p="10px">並び順</Box>
+        <Box p="2px">
           <Menu>
-            <MenuButton px={4} py={2} transition="all 0.2s" borderRadius="md" _hover={{ bg: "gray.400" }} _expanded={{ bg: "gray.400" }} >
+            <MenuButton px={4} py={2} transition="all 0.2s" border="1px solid black" borderRadius="md" borderColor={borderColor} _hover={{ bg: "gray.400" }} _expanded={{ bg: "gray.400" }} >
               新しい順 <ChevronDownIcon />
             </MenuButton>
             <MenuList>
