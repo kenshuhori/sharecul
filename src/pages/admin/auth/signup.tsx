a8eceed954f3a8fabdedb517c24ea1c095643063
KenshuHori <kenshuhori@gmail.com>
2021-11-07 15:16:56 +0900

    パスワードでサインインを試す

--- src/pages/admin/auth/signup.tsx
+++ src/pages/admin/auth/signup.tsx
@@ -1,53 +1,51 @@
 import { useState } from 'react'
-import { Flex, Heading, FormControl, FormLabel, FormErrorMessage, FormHelperText, InputGroup, InputLeftElement, InputRightElement, Text, Input, Button, useColorModeValue } from '@chakra-ui/react'
-import { Formik, FormikProps, Form, Field, ErrorMessage } from 'formik';
+import {
+  Button,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { supabase } from '@/utils/supabase'
 import { InputChecker } from '@/components/utils/InputChecker'
 import api from '@/utils/api'
 
 const IndexPage = () => {
   const formBackground = useColorModeValue("gray.100", "gray.700");
-  const handleChangeName = (event) => setName(event.target.value);
-  const handleChangeEmail = (event) => setEmail(event.target.value);
-  const handleChangePassword = (event) => setPassword(event.target.value);
-  const handleChangePasswordConfirm = (event) => setPasswordConfirm(event.target.value);
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password_confirm, setPasswordConfirm] = useState("");
-  const submit = async event => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
-    const res = await api.post('/api/admin',
-      {
-        name: event.target.name.value,
-        email: event.target.email.value,
-        password: event.target.password.value
-      }
-    )
+    let email = event.target.email.value
+    let password = event.target.password.value
+    const res = await supabase.auth.signUp({
+      email,
+      password,
+    });
     console.log(res)
   }
   return (
-    <form onSubmit={submit}>
+    <form onSubmit={handleSubmit}>
       <Flex alignItems="center" justifyContent="center" m={8}>
         <Flex direction="column" background={formBackground} p={12}>
-          <Heading mb={6}>Sign Up</Heading>
-          <Text fontSize={12}>お名前</Text>
-          <InputGroup>
-            <Input placeholder="田中たろう" varient="Filled" mb={3} type="text" value={name} onChange={handleChangeName} />
-            <InputRightElement children={<InputChecker type="text" value={name} />} />
-          </InputGroup>
+          <Heading mb={6}>アカウント登録</Heading>
           <Text fontSize={12}>メールアドレス</Text>
           <InputGroup>
-            <Input placeholder="sharecul@example.com" varient="Filled" mb={3} type="email" value={email} onChange={handleChangeEmail} />
+            <Input name="email" placeholder="sharecul@example.com" varient="Filled" mb={3} type="email" value={email} onChange={(e) => {setEmail(e.target.value)}} />
             <InputRightElement children={<InputChecker type="email" value={email} />} />
           </InputGroup>
           <Text fontSize={12}>パスワード</Text>
           <InputGroup>
-            <Input placeholder="******" varient="Fileed" mb={6} type="password" value={password} onChange={handleChangePassword} />
+            <Input name="password" placeholder="******" varient="Fileed" mb={6} type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
             <InputRightElement children={<InputChecker type="password" value={password} />} />
           </InputGroup>
           <Text fontSize={12}>パスワード確認用</Text>
           <InputGroup>
-            <Input placeholder="******" varient="Fileed" mb={6} type="password" value={password_confirm} onChange={handleChangePasswordConfirm} />
+            <Input placeholder="******" varient="Fileed" mb={6} type="password" value={password_confirm} onChange={(e) => {setPasswordConfirm(e.target.value)}} />
             <InputRightElement children={<InputChecker type="password_confirm" value={!!password_confirm && password == password_confirm ? 'true' : 'false'} />} />
           </InputGroup>
           <Button type="submit" colorScheme="teal" mb={4}>登録</Button>
