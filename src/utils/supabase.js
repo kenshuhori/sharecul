7eec4fcd7201b45f0fb0633c436d97d45e17172d
KenshuHori <kenshuhori@gmail.com>
2021-11-13 13:48:57 +0900

    jsに変更する

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -2,24 +2,20 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
 
-interface SupabaseEq {
-  column: string | number | symbol,
-  value: string
-}
-export async function readAll (tablename: string) {
+export async function readAll (tablename) {
   let { data: results, error } = await supabase
     .from(tablename)
     .select('*');
   return results;
 };
-export async function read (tablename: string, eq: SupabaseEq) {
+export async function read (tablename, eq) {
   let { data: results, error } = await supabase
     .from(tablename)
     .select('*')
     .eq(eq.column, eq.value);
   return results;
 };
-export async function upsertRow (tablename: string, values: Object) {
+export async function upsertRow (tablename, values) {
   const { data, error } = await supabase
     .from(tablename)
     .upsert([values]);
