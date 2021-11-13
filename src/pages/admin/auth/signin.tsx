93d0ed7685b7621dae2e4d10aadba6ebaccc9bc0
KenshuHori <kenshuhori@gmail.com>
2021-11-13 10:47:09 +0900

    テスト用に作成した不要なページを削除

--- src/pages/admin/auth/signin.tsx
+++ src/pages/admin/auth/signin.tsx
@@ -1,22 +1,7 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '@/utils/supabase';
-import Auth from '@/components/admin/auth/Auth';
-import Account from '@/components/admin/auth/Account';
 
-export default function Home() {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    setSession(supabase.auth.session());
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
+export default function AdminAuthSigninPage() {
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
-    </div>
+    <div>Signin</div>
   );
 }
